ae63943223fbddb5a055b4991ac686b892d37623
Inge <inge.becht91@gmail.com>
2012-09-04 09:31:54 +0200

    added first assignment

---
+++ Assignment1/ass1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    
+    // Unsigned char byte is the size of a byte?
+    unsigned char byte;
+    printf("The size of a byte: %d\n", sizeof(byte));
+    printf("Calculating machine epsilon: ")
+    return 0;
+}
