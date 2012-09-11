06104a84a842fbb53010003789f77fa9cc81580a
Inge <inge.becht91@gmail.com>
2012-09-11 09:42:38 +0200

    added main and first exercise in ass2.cpp

---
+++ Assignment2/ass2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <float.h>
+
+using namespace std;
+
+
+void main(){
+
+}
+
+
+// Finding the slope of two points using finite distance approach
+void ass2_1(){
+
+    //First is the choice of h (the increment)
+    h =
+
+}
