721f7d30bf3bb2f8974aabe9ad2aa497285e074e
Inge <inge.becht91@gmail.com>
2012-09-11 13:21:27 +0200

    added sin function

--- Assignment2/ass2.cpp
+++ Assignment2/ass2.cpp
@@ -6,13 +6,26 @@ using namespace std;
 
 void main(){
 
+        ass2_1( &sin
 }
 
 
 // Finding the slope of two points using finite distance approach
-void ass2_1(double x){
+void ass2_1(double (*f)(double), double x)
+{
 
-    //First is the choice of h (the increment)
+    double phx;
+    double dx;
+    // First is the choice of h (the increment)
     h = sqrt(DBL_EPSILON) * x;
+    xph += (x + h);
+    slope = ;
 
+}
+
+
+// The sin function that is needed for the first example
+double sin(double x){
+    
+    return sin(x);
 }
