424eaa5bebc2369f82c2c1d1fde95220f9e0cc78
Inge <inge.becht91@gmail.com>
2012-09-11 09:48:09 +0200

    added file

--- Assignment2/ass2.cpp
+++ Assignment2/ass2.cpp
@@ -10,9 +10,9 @@ void main(){
 
 
 // Finding the slope of two points using finite distance approach
-void ass2_1(){
+void ass2_1(double x){
 
     //First is the choice of h (the increment)
-    h =
+    h = sqrt(DBL_EPSILON) * x;
 
 }
