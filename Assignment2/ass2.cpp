0503372330ccfdf3090e38a611cf1cead3d692ae
Inge <inge.becht91@gmail.com>
2012-09-11 18:17:37 +0200

    added values to test for sin(x)

--- Assignment2/ass2.cpp
+++ Assignment2/ass2.cpp
@@ -1,26 +1,37 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <float.h>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
 
-void main(){
 
-        ass2_1( &sin
+void main()
+{
+    // different values for x to try
+    double sin_x[] = {M_PI / 3, 100 * M_PI + M_PI/3, pow(10, 12) * M_PI + M_PI / 3};
+
+    for(int i = 0; i < length(sin_x); i ++ ){
+
+    }
+
 }
 
 
+
 // Finding the slope of two points using finite distance approach
-void ass2_1(double (*f)(double), double x)
+void numerical_differentation(double (*f)(double), double x)
 {
 
     double phx;
     double dx;
     // First is the choice of h (the increment)
+    // smallest possible interval around x
     h = sqrt(DBL_EPSILON) * x;
     xph += (x + h);
-    slope = ;
-
+    slope = ((*f)(xph) -  (*f)(x)) / dx ;
 }
 
 
