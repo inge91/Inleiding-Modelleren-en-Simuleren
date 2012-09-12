2c73c304c9d3f6a3bc5d93f058997abf36c1968e
Inge <inge.becht91@gmail.com>
2012-09-12 14:00:23 +0200

    added table print

--- Assignment2/ass2.cpp
+++ Assignment2/ass2.cpp
@@ -7,14 +7,31 @@
 #include <map>
 
 using namespace std;
+
+struct approx_slope{
+    void set_value(double app, double act){
+        approx_deriv = app;
+        actual_deriv = act;
+        error = abs(app - act);
+    }
+
+    double approx_deriv;
+    double actual_deriv;
+    double error;
+};
+
+
 double righthand_side_differentiation(double (*f)(double), double x, double h);
 double square( double x);
 double get_h_value(double *sin_x, int i, int j);
 double sin_function(double x);
-void print_map(map<double, map<double, double> > mappie);
+void print_map(map<string, map<double, approx_slope> > mappie);
+
+
 
 int main()
 {
+    std::cout<<"Calculating the right hand numerical derivative"<<endl;
     // different values for x to try
     double sin_x[] = {M_PI / 3, 100 * M_PI + M_PI/3,
                       pow(10, 12) * M_PI + M_PI / 3};
@@ -32,10 +49,10 @@ int main()
     int h_size = sizeof values_h / sizeof(double);
 
     // Making a map with first key x, second key h
-    map<double, map<double, double> > map_x_h_slope;
+    map<string, map<double, approx_slope> > map_x_h_slope;
 
 
-    double slope;
+    double approx_deriv;
 
     // Calculate all slopes using the vector values aboven
     for(int i = 0; i < x_size; i++){
@@ -44,9 +61,10 @@ int main()
         for(int j = 0; j < h_size; j ++ ){
 
             // Calculate slope
-            slope = righthand_side_differentiation(sin_function, sin_x[i],
+            approx_deriv = righthand_side_differentiation(sin_function, sin_x[i],
                                                    values_h[j]);
-            map_x_h_slope[sin_x[i]][values_h[j]] = slope;
+            map_x_h_slope[str_array[i]][values_h[j]].set_value(approx_deriv,
+                                                                actual_derivative[i]);
 
         }
     }
@@ -54,40 +72,32 @@ int main()
     // Add special case for the Wikpedia suggestion of sqrt(epsilon) * x
     for(int i = 0; i < x_size; i++){
         double h_value = sqrt(DBL_EPSILON) * sin_x[i];
-        slope = righthand_side_differentiation(sin_function, sin_x[i], h_value);
-        map_x_h_slope[sin_x[i]][h_value] = slope;
-    }
-/*
-    printf("-----differentiating for different values of f(x) = sin(x)------\n");
-    printf("value h \t\t value x \t\t outcome \t\t actual outcome"
-           "\t\terror\n\n");
-    printf("%.12f\t\t",get_h_value(sin_x, i, j));
-    printf("%s\t\t", str_array[i].c_str());
-    printf("%.12f\t\t", slope);
-    printf("%.12f\t\t", actual_derivative[i]);
-    printf("%.12f\t\t", abs(actual_derivative[i] - slope));
+        approx_deriv = righthand_side_differentiation(sin_function, sin_x[i], h_value);
+        map_x_h_slope[str_array[i]][h_value].set_value(approx_deriv,
+                                                            actual_derivative[i]);
 
-    return 0;
-*/
+    }
+
 
     print_map(map_x_h_slope);
 }
 
 
 // Print the table
-void print_map(map<double, map<double, double> > mappie)
+void print_map(map<string, map<double, approx_slope> > mappie)
 {
     printf("-----differentiating for different values of f(x) = sin(x)------\n");
-    printf("value h \t\t value x \t\t outcome \t\t actual outcome"
-           "\t\terror\n\n");
-    map<double, map<double, double> >::iterator it;
-    map<double, double>::iterator it2;
+    printf("%*s, %*s, %*s, %*s, %*s\n", 20, "value h", 16, "value x", 16, "outcome", 16,
+           "actual outcome", 16, "error");
+    map<string, map<double, approx_slope> >::iterator it;
+    map<double, approx_slope>::iterator it2;
     for(it = mappie.begin(); it != mappie.end(); it++)
     {
         for(it2 = it->second.begin(); it2 != it->second.end(); it2++)
         {
-            printf("%.12f, ---- %.12f ----- %.12f\n", it->first,
-                   it2->first, it2->second);
+            printf("%*s, %*.12f %*.12f %*.12f %*.12f\n", 20, it->first.c_str(),
+                   16, it2->first, 16, it2->second.approx_deriv, 16,
+                   it2->second.actual_deriv, 16,it2->second.error);
         }
     }
 
