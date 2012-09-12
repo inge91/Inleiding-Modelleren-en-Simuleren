231d231fe4b996c7234a2656c7cac362476a54cf
Maarten de Jonge <mdejonge1990@gmail.com>
2012-09-12 21:48:21 +0200

    update x_intersection code

--- Assignment2/x_intersection.cpp
+++ Assignment2/x_intersection.cpp
@@ -1,4 +1,8 @@
+#ifndef X_INTERSECTION_H
+#define X_INTERSECTION_H
+
 #include <iostream>
+#include <fstream>
 
 #include "x_intersection.h"
 
@@ -9,13 +13,19 @@ using namespace std;
 double bisection(double (*f)(double), double a, double b)
 {
     double midpoint = 0;
-    double threshold = 0.00000001; // used to approximate zero
+    double threshold = 0.00000000001; // used to approximate zero
+
+    ofstream data_file("bisection.txt", ios::trunc | ios::out);
 
     // limit the amount of iterations
     for(int max_iter = 0; max_iter < 1000; ++max_iter) {
         midpoint = (a + b) / 2.0;
 
+        // log data for plotting purposes
+        log_data(data_file, max_iter, (*f)(midpoint));
+
         if ((*f)(midpoint) > -threshold && (*f)(midpoint) < threshold) {
+            data_file.close();
             return midpoint;
         }
 
@@ -28,6 +38,7 @@ double bisection(double (*f)(double), double a, double b)
 
     // if no solution has been found in the maximum number of iteration, return
     // the latest value and hope for the best
+    data_file.close();
     return midpoint;
 }
 
@@ -37,6 +48,8 @@ double x_squared_minus_two(double x)
     return (x * x) - 2;
 }
 
+// returns true if both x and y have the same sign (i.e. both positive, both
+// negative, or both zero)
 bool same_sign(double x, double y)
 {
     if ((x < 0) && (y < 0))
@@ -48,3 +61,11 @@ bool same_sign(double x, double y)
 
     return false;
 }
+
+// logs datapoints into a file using a CSV format
+void log_data(ofstream &file, double x, double y)
+{
+    file << x << ", " << y << endl;
+}
+
+#endif
--- Assignment2/x_intersection.h
+++ Assignment2/x_intersection.h
@@ -1,3 +1,8 @@
+#include <fstream>
+
 double x_squared_minus_two(double x);
 double bisection(double (*f)(double), double a, double b);
+
 bool same_sign(double x, double y);
+
+void log_data(std::ofstream &file, double x, double y);
