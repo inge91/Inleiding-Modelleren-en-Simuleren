b1433a43be19bbe08f3b7b74e537830725933ce7
Inge <inge.becht91@gmail.com>
2012-09-27 21:56:18 +0200

    done with new ringe kutta 4

--- Test/rk.c
+++ Test/rk.c
@@ -4,9 +4,14 @@
 
 FILE *output;              /* internal filename */
 
-void runge4(double t0, double t1, double dt, double y0[], double y1[], int N, int f(double, double *, double*, int) );   /* Runge-Kutta function */
+typedef struct
+{
+    int N;
+} param_struct;
+
+void runge4(double t0, double t1, double dt, double y0[], double y1[], int N, int f(double, double *, double*, void *), void *params );   /* Runge-Kutta function */
 double f4(double t, double y[]);            /* function for derivatives */
-int f42(double t, double * y, double * dy, int N);
+int f42(double t, double * y, double * dy, void *);
 
 void main()
 {
@@ -15,13 +20,16 @@ void main()
 
     y0[0]= 1.0;                                      /* initial position */
 
+    param_struct *ps = (param_struct *) malloc( sizeof(param_struct) );
+    ps->N = 1;
 
-    runge4(0.0, 5.0,  0.01, y0, y1,  N, f42);
+    runge4(0.0, 5.0,  4, y0, y1,  N, f42, ps);
 
     printf("y1L %f \n ", y1[0]);
 
 }
-void runge4(double t0, double t1, double dt, double * y0, double * y1, int N, int f(double, double *, double *, int) )
+void runge4(double t0, double t1, double dt, double * y0, double * y1, int N, int f(double, double *, double *, void *),
+          void * params )
 {
 
     // because the loop starts at y_n+1, use the values of y0 as initial values
@@ -39,49 +47,49 @@ void runge4(double t0, double t1, double dt, double * y0, double * y1, int N, in
 
     for (double t = t0 + dt; t<=t1 ; t += dt)                     
     {
-        f(t, yt, k1, N);
-        for(i=0;i<N;i++) 
+        f(t, yt, k1, params);
+        for(i = 0; i < N; i++) 
         {
-            k1[i]*=dt;
+            k1[i] *= dt;
         }
 
-        for(i=0;i<N;i++) 
+        for(i = 0; i < N; i++) 
         {
-            temp1[i]=yt[i]+0.5*k1[i];
+            temp1[i] = yt[i] + 0.5 * k1[i];
         }
 
-        f(t+(dt * 0.5), temp1, k2, N);
+        f(t+(dt * 0.5), temp1, k2, params);
 
-        for (i=0;i<N;i++) 
+        for (i = 0; i < N; i++) 
         {
-            k2[i]*=dt;
+            k2[i] *= dt;
         }
 
-        for (i=0;i<N;i++) 
+        for (i = 0; i < N; i++) 
         {
-            temp2[i]=yt[i]+0.5*k2[i];
+            temp2[i] = yt[i] + 0.5 * k2[i];
         }
 
-        f(t + (dt * 0.5), temp2, k3, N);
+        f(t + (dt * 0.5), temp2, k3, params);
 
-        for (i=0;i<N;i++) {
-            k3[i]*=dt;
+        for (i = 0; i < N; i++) {
+            k3[i] *= dt;
         }
 
-        for (i=0;i<N;i++) {
-            temp3[i]=yt[i]+ k3[i];
+        for (i = 0; i < N; i++) {
+            temp3[i] = yt[i] + k3[i];
         }
 
-        f(t + dt, temp3, k4, N);
+        f(t + dt, temp3, k4, params);
         
-        for (i=0;i<N;i++) 
+        for (i = 0; i < N; i++) 
         {
-            k4[i]*=dt;
+            k4[i] *= dt;
         }
 
-        for (i=0;i<N;i++) 
+        for (i=0; i<N; i++) 
         {
-            yt[i]+=(k1[i]+2*k2[i]+2*k3[i]+k4[i])/6.0;
+            yt[i]+=( k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i])/6.0;
         }
         printf("%f\n",yt[0]);
     }
@@ -93,9 +101,9 @@ void runge4(double t0, double t1, double dt, double * y0, double * y1, int N, in
         }
 }
 
-int f42(double t, double * y, double * dy, int N)
+int f42(double t, double * y, double * dy, void *params)
 {
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < ((param_struct*)params)->N; i++){
         dy[i] = y[i];
     }
     return 0;
