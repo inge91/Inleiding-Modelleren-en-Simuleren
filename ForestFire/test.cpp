9a8c50b941c9de6529e14f1cbb727bcc8c68ff0c
Maarten de Jonge <mdejonge1990@gmail.com>
2012-10-15 14:47:13 +0200

    fix data visualisation

--- ForestFire/test.cpp
+++ ForestFire/test.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main(int argc, const char *argv[])
 {
     vector<int> sizes = {10, 15, 20, 25, 30, 35};
-    vector<double> densities = {0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.8};
-    int repeat = 50;
+    vector<double> densities = {0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
+    int repeat = 250;
 
     // save the data
     ofstream file("data.txt");
