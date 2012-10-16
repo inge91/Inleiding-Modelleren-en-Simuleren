2d1126b99c93ac720d2f0e891607d6bd902f6eeb
Maarten de Jonge <mdejonge1990@gmail.com>
2012-10-16 14:29:21 +0200

    update report with new plot

--- ForestFire/data_visualisation.py
+++ ForestFire/data_visualisation.py
@@ -2,6 +2,8 @@
 # encoding: utf-8
 
 from pylab import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # columns in the data correspond to:
 # size | density | other side reached (boolean) | steps to other side (0 if not
@@ -45,4 +47,18 @@ ylabel("% of vegetation that was burnt")
 xlim(0, 1)
 ylim(0, 102)
 savefig("density_vs_burnt.png")
+clf()
+
+# sizes against number of steps
+
+# filter the lists to only plot the densities from 0.6 to 0.9
+szs = [size for (size, density) in zip(sizes, densities) if 0.6 <= density <= 0.9]
+sts = [step for (step, density) in zip(steps, densities) if 0.6 <= density <= 0.9]
+hexbin(szs, sts, gridsize=30, mincnt=1)
+cb = colorbar()
+cb.set_label("amount of data points")
+xlabel("size")
+ylabel("Steps to reach other side")
+
+savefig("size_vs_steps.png")
 clf()
